6604b4a7b5793c0bd87eb28054702fc9b0755e8c
Rashaad <wadeness23@yahoo.com>
2023-05-27 19:20:10 -0400

    adding fibinocci code

---
+++ src/main/java/testpackage/App.java
@@ -0,0 +1,26 @@
+package testpackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int n = 10;
+        System.out.println("Fibonacci Series ");
+        for(int i=0;i<n;i++) {
+        	System.out.println(generateFibonnacci(i));
+        }
+    }
+    public static int generateFibonnacci(int k) {
+    	if(k<=1) {
+    		return k;
+    	}
+    		else {
+    			return generateFibonnacci(k-1) + generateFibonnacci(k-2);
+    		}
+    	}
+    }
+
